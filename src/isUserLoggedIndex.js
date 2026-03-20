9441964b702a3db17f11ffff1d870cc5a9277dd6
Flo <227476052+flo-in-code@users.noreply.github.com>
2026-03-20 01:52:36 -0700

    Update login/logout button to change depending on whether the user is logged in or out

--- src/isUserLoggedIndex.js
+++ src/isUserLoggedIndex.js
@@ -1,5 +1,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "/src/firebaseConfig.js";
+import { logoutUser } from "./authentication";
+import { log } from "firebase/firestore/pipelines";
 
 // Simple function that hides create event button if user in not logged in on index.html.
 // For whatever reason, if a user that's been logged in ends up back on index.html it will show up again.
@@ -11,4 +13,28 @@ function createEventDisplayCheck() {
   });
 }
 
+function updateLoginLogoutButton() {
+  onAuthStateChanged (auth, (user) => {
+    const loginLogoutButton = document.getElementById("login-logout-button")
+    if(!loginLogoutButton){
+      console.log("button in navbar has not loaded yet")
+      return
+    }
+
+    if (user) { // user is logged in, so we need to show logout
+      loginLogoutButton.href = "#"
+      loginLogoutButton.querySelector("div").textContent = "Log out"
+      loginLogoutButton.addEventListener("click", (e) => {
+        e.preventDefault()
+        logoutUser();
+      });
+    } else{ // user is NOT logged in, so we need to show log in
+        loginLogoutButton.href = "/login.html"
+        loginLogoutButton.querySelector("div").textContent = "Log in"
+    }
+  })
+}
+
+
 createEventDisplayCheck();
+updateLoginLogoutButton();
